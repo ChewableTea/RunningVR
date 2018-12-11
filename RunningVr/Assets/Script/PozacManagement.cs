3acedee0f6a1e75726e309d9c0df241a6b650c0d
ButterKuma <42940018+ButterKuma@users.noreply.github.com>
2018-12-12 00:20:48 +0900

    update2

---
+++ RunningVr/Assets/Script/PozacManagement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PozacManagement : MonoBehaviour {
+    public GameObject Dest;
+    NavMeshAgent nav;
+	// Use this for initialization
+	void Awake () {
+        Dest = GameObject.FindGameObjectWithTag("Destination");
+
+        nav = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        nav.destination = Dest.transform.position;
+
+        if (nav.velocity.sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.rotation = Quaternion.LookRotation(nav.velocity.normalized);
+        }
+    }
+}
